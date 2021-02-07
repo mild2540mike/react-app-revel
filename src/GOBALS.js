e9624c00cbbb0cb6f0927c8cd6083daca2bbcd1c
Yanaranop Naulrahaeng <yanaranop2540mike@hotmail.com>
2021-02-07 20:48:17 +0700

    Gobal

--- src/GOBALS.js
+++ src/GOBALS.js
@@ -1,7 +1,12 @@
 
 export default {
-	URL: 'http://localhost:5001/',
-	URL_IMG: 'http://localhost:5001/',
-	URL_UPLOAD: 'http://localhost:5001/upload-image/',
-	URL_DELETE: 'http://localhost:5001/delete-image/',
+	// URL: 'http://localhost:5001/',
+	// URL_IMG: 'http://localhost:5001/',
+	// URL_UPLOAD: 'http://localhost:5001/upload-image/',
+	// URL_DELETE: 'http://localhost:5001/delete-image/',
+
+	URL: 'https://react-mike-dev.herokuapp.com/',
+	URL_IMG: 'https://react-mike-dev.herokuapp.com/',
+	URL_UPLOAD: 'https://react-mike-dev.herokuapp.com/upload-image/',
+	URL_DELETE: 'https://react-mike-dev.herokuapp.com/delete-image/',
 }
